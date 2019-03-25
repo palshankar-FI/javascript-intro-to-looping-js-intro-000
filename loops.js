cb514daaeba5690680646c36d20b5580bbc8edc2
PS <palshankar@outlook.com>
2019-03-25 09:05:20 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,3 +3,23 @@ function forLoop(array) {
     array.push(`"I am ${i} strange loop${i === 0 ?  : s}."`);
   }
 }
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(`${n}`);
+    n -= 1;
+  }
+  return('done');
+}
+
+describe('whileLoop(n)', () => {
+  it('counts down from n to 0', () => {
+    const spy = chai.spy.on(console, 'log')
+    const n = Math.floor(Math.random() * 100)
+
+    expect(whileLoop(n)).to.equal('done')
+    expect(spy).to.have.been.called.exactly(n)
+
+    console.log.reset()
+  })
+})
